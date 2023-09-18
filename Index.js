3516f5d46e72a8065046dbe3218807e90b65386a
Romain <profurax@gmail.com>
2023-09-18 03:10:43 +0200

    add slide bar

--- Index.js
+++ Index.js
@@ -14,7 +14,6 @@ car1.addEventListener("click", () => {
   resetCarsBorders();
   car1.style.border = "1.5px solid #E3000B";
   car1.classList.add("cart1-animation");
-  
 });
 
 car2.addEventListener("click", () => {
@@ -25,4 +24,18 @@ car2.addEventListener("click", () => {
 car3.addEventListener("click", () => {
   resetCarsBorders();
   car3.style.border = "1.5px solid #E3000B";
+});
+const menuButton = document.getElementById("menu");
+const slideBar = document.querySelector(".Slider-bar");
+const closeButton = document.getElementById("close");
+const navSlider = document.querySelector(".Slider-nav");
+
+menu.addEventListener("click", () => {
+  slideBar.style.visibility = "visible";
+});
+closeButton.addEventListener("click", () => {
+  slideBar.style.visibility = "hidden";
+});
+navSlider.addEventListener("click", () => {
+  slideBar.style.visibility = "hidden";
 });
